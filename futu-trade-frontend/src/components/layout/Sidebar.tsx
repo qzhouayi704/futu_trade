60f2eae10cb0caffae0050ddec0778ae4e3bc032
ZHOUYICAN <648177665@qq.com>
2026-06-12 10:58:57 +0800

    fix: 恢复交易驾驶舱导航入口

--- futu-trade-frontend/src/components/layout/Sidebar.tsx
+++ futu-trade-frontend/src/components/layout/Sidebar.tsx
@@ -38,6 +38,11 @@ const navGroups: NavGroup[] = [
         path: "/",
         icon: <Icon d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6" />,
       },
+      {
+        name: "交易驾驶舱",
+        path: "/trading",
+        icon: <Icon d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />,
+      },
     ],
   },
   {
